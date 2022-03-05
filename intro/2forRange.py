287845818b5ca30ab955e32ff0ea96eb194fe62d
Shan Jia <73159723+Js11252001@users.noreply.github.com>
2022-03-05 15:16:04 +0800

    print random integers

--- intro/2forRange.py
+++ intro/2forRange.py
@@ -1,4 +1,7 @@
 import random
 list = []
 for i in range(10):
-    print(i, end=", ")
+    # print(i, end=", ")
+    list.append(random.randrange(100)) # Integer from 0 to 99 inclusive
+    
+print(list)
