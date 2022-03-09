ab516a0b45ec9dcf27e9a5ff80a149a6cc70b2ec
Shan Jia <73159723+Js11252001@users.noreply.github.com>
2022-03-09 14:27:39 +0800

    fix quit bug

--- intro/6forFunction.py
+++ intro/6forFunction.py
@@ -1,12 +1,13 @@
 def Celcius2Fahrenheit(celcius):
     return (celcius * 1.8) + 32
 
-isrunning = True
+
 if __name__ == "__main__":
+    isrunning = True
     while isrunning:
         str = input("Convert celcius to fahrenheit(input q to quit)")
         if str.lower() == "q":
-            isRunning = False
+            isrunning = False
         else:
             cel = float(str)
             print("fahrenheit:", Celcius2Fahrenheit(cel))
