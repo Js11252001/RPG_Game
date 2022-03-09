0d3505e283a58efa6abc6fb60d1b5575126da567
Shan Jia <73159723+Js11252001@users.noreply.github.com>
2022-03-09 14:24:17 +0800

    Converts celcius to fahrenheit

--- intro/6forFunction.py
+++ intro/6forFunction.py
@@ -1,2 +1,12 @@
 def Celcius2Fahrenheit(celcius):
     return (celcius * 1.8) + 32
+
+isrunning = True
+if __name__ == "__main__":
+    while isrunning:
+        str = input("Convert celcius to fahrenheit(input q to quit)")
+        if str.lower() == "q":
+            isRunning = False
+        else:
+            cel = float(str)
+            print("fahrenheit:", Celcius2Fahrenheit(cel))
