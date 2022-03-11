ebcd6b4a44e9883aab99c9e489ec4fa754da2096
js11250014 <2680669149@qq.com>
2022-03-11 08:44:10 +0800

    add methods

--- intro/7forClass.py
+++ intro/7forClass.py
@@ -1,3 +1,5 @@
+import random
+
 class Character:
     def __init__(self, strength, dexterity, constitution, intelligence, wisdom, charisma):
         self.strength = strength
@@ -18,3 +20,16 @@ class Character:
 
     def printHitpoints(self):
         print("hitpoints: ", self.hitpoints)
+
+    def attack(self):
+        return random.randrange(1, self.strength + 1)
+
+    def defense(self, defen):
+        defen = random.randrange(1, 21)
+        if defen >= self.dexterity:
+            defen - self.attack()
+    
+    def heal(self, val):
+        self.hitpoints += val
+
+
