5690990d04c3e0c4a84c42b1404c44ec79409317
Shan Jia <73159723+Js11252001@users.noreply.github.com>
2022-03-18 15:48:36 +0800

    Create copy of 1ball

---
+++ ballTutorial/moveWithKey.py
@@ -0,0 +1,28 @@
+import sys, pygame
+from item import *
+pygame.init()
+
+size = width, height = 640, 480
+faceSpeed = [2, 2]
+ballSpeed = [1, 1]
+gray = (156, 156, 156)
+white = (255, 255, 255)
+
+screen = pygame.display.set_mode(size)
+
+ball = Item("intro_ball.gif", [2,2], size[0], size[1])
+face = Item("smiley.png", [1,1], size[0], size[1])
+
+isRunning = True
+while isRunning:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT: sys.exit()
+
+  ball.move()
+  face.move()
+
+  screen.fill(white)
+
+  screen.blit(ball.object, ball.rect)
+  screen.blit(face.object, face.rect)
+  pygame.display.flip()
