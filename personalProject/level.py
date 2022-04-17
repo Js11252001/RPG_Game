91db03519119fb20efa7442033e5483437835dcd
js11250014 <2680669149@qq.com>
2022-04-17 10:56:10 +0800

    set the basis of level
    add basis level class to prepare for the level design

---
+++ personalProject/level.py
@@ -0,0 +1,31 @@
+import pygame
+from setting import *
+from tile import Tile
+from player import Player
+
+class Level:
+    def __init__(self):
+
+        # get the display surface
+        self.display_surface = pygame.display.get_surface()
+
+        # spriteGruop setup
+        self.visibleSprite = pygame.sprite.Group()
+        self.obstacleSprite = pygame.sprite.Group()
+
+        # sprite setup
+        self.createMap()
+
+    def createMap(self):
+        for rowIndex, row in enumerate(WORLD_MAP):
+            for colIndex, col in enumerate(row):
+                x = colIndex * TILESIZE
+                y = rowIndex * TILESIZE
+                if col == 'x':
+                    Tile((x,y), [self.visibleSprite, self.obstacleSprite])
+                if col == 'p':
+                    Player((x,y), [self.visibleSprite])
+
+    def run(self):
+        # update & draw
+        self.visibleSprite.draw(self.display_surface)
